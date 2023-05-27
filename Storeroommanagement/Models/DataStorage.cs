822ebb44b878f1980717928e22514a7ae6a0fe33
fatemefarokhzad98 <fatemefarokhzad98@gmail.com>
2023-05-28 02:01:43 +0430

    Add List DataModel

--- Storeroommanagement/Models/DataStorage.cs
+++ Storeroommanagement/Models/DataStorage.cs
@@ -9,8 +9,49 @@
             {
                 Id=1,
                 Model="A10",
+                Color="black",
+                Brand=BrandEnum.Brand.Asus,
+                Groups=GroupsEnum.Groups.LopTop
 
-            }
+
+            },
+             new StorageModel()
+
+            {
+                Id=2,
+                Model="A30",
+                Color="green",
+                Brand=BrandEnum.Brand.Samsung,
+                Groups=GroupsEnum.Groups.Mobile
+
+
+            },
+              new StorageModel()
+
+            {
+                Id=3,
+                Model="A50",
+                Color="blue",
+                Brand=BrandEnum.Brand.Lenovo,
+                Groups=GroupsEnum.Groups.LopTop
+
+
+            },
+                  new StorageModel()
+
+            {
+                Id=4,
+                Model="A70",
+                Color="yellow",
+                Brand=BrandEnum.Brand.Huawi,
+                Groups=GroupsEnum.Groups.Mobile
+
+
+            },
+
+
+
+
         };
 
       
--- Storeroommanagement/Models/StorageModel.cs
+++ Storeroommanagement/Models/StorageModel.cs
@@ -2,7 +2,7 @@
 {
     public class StorageModel
     {
-        public string Name { get; set; }
+      
         public int Id { get; set; }
         public string Color { get; set; }
         public string Model { get; set; }
